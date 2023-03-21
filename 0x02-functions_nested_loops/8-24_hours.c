0ccd88600caaadcef49d7fb8eae76cdc804a3a81
mastax <elbahtouri.m@gmail.com>
2023-03-21 08:48:32 -0700

    8-24_hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * jack_bauer - Prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+ *
+ * Return: no return.
+ */
+void jack_bauer(void);
+{
+	int a, b, c, d;
+
+	for (a = 0; a <= 2; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			for (c = 0; c <= 5; c++)
+			{
+				for (d = 0; d <= 9; d++)
+				{
+					if (a >= 2 && b >= 4)
+						break;
+					_putchat(a + 48);
+					_putchat(b + 48);
+					_putchat(58);
+					_putchat(c + 48);
+					_putchat(d + 48);
+					_putchat('\n');
+				}
+			}
+		}
+	}
+}
