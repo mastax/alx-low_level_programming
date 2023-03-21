c33cb7ce24f44476aa3eb8c3b14c7a68fa574204
mastax <elbahtouri.m@gmail.com>
2023-03-21 08:02:02 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _abs - Compute the absolute value of integer.
+ *
+ * @i: input number as integer.
+ * @int: input number as integer.
+ *
+ * Return: Absolute value.
+ */
+int _abs(int)
+{
+	int i;
+
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
