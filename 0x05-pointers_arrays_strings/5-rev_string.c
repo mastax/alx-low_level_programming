267686d6aac90a7ec068a594a5e451d1f1efc853
mastax <elbahtouri.m@gmail.com>
2023-03-27 14:36:08 -0700

    rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string.
+ * @s: input string.
+ * Return: String in inverse
+ */
+void rev_string(char *s)
+{
+	char rev;
+	int i;
+	int count;
+
+	rev = a[0];
+	count = 0;
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	for (i = 0; i < count; i++)
+	{
+		count--;
+		rev = s[i];
+		s[i] = s[count];
+		s[count] = rev;
+	}
+}
