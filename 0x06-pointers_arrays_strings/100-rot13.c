cdbc065265a574b0ef97a1ca1138126433e87a28
mastax <elbahtouri.m@gmail.com>
2023-03-29 23:24:18 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * rot13 - change letters to ROT13.
+ * @s: analized string.
+ *
+ * Return: String with all letters in ROT13 base.
+ */
+char *rot13(char *s)
+{
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i = 0;
+	int j = 0;
+
+	while (*(s + i) != '\0')
+	{
+		j = 0;
+		while (j <= 51)
+		{
+			if (*(s + i) == a[j])
+			{
+				*(s + i) = rot[j];
+				break
+			}
+			j++;
+		}
+		i++;
+	}
+	return (s);
+}
