16efc06a44953ca319bd48953014213b43fe8ef0
mastax <elbahtouri.m@gmail.com>
2023-04-05 00:41:20 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,10 +1,10 @@
 #include "main.h"
-int square_root(int n, int low, int high);
 /**
- * _sqrt_recursion - Calculates the natural square root of a number.
- * @n: The number to calculate the square root of.
- * Return: The natural square root of n, or -1 if n does not have a
- * natural square root.
+ * _sqrt_recursion - return the sqrt of a natural number.
+ * @n: natural number
+ *
+ *
+ * Return: sqrt 0 if sqrt is not natural.
  */
 int _sqrt_recursion(int n)
 {
@@ -12,35 +12,34 @@ int _sqrt_recursion(int n)
 	{
 		return (-1);
 	}
-	return (square_root(n, 0, n));
-}
-/**
- * square_root - Helper function for _sqrt_recursion.
- *
- * @n: The number to calculate the square root of.
- * @low: The lowest possible square root of n.
- * @high: The highest possible square root of n.
- * Return: The natural square root of n, or -1 if n does not have a
- * natural square root.
- */
-int square_root(int n, int low, int high)
-{
-	int mid = (low + high) / 2;
-
-	if (low > high)
+	else if (n == 0 || n == 1)
 	{
-		return (-1);
-	}
-	if (mid * mid == n)
-	{
-		return (mid);
-	}
-	else if (mid * mid > n)
-	{
-		return (square_root(n, low, mid - 1));
+		return (n);
 	}
 	else
 	{
-		return (square_root(n, mid + 1, high));
+		return (sqrt_manual(n, 1));
+	}
+}
+/**
+ * sqrt_manual - calculate manualy the sqrt of a number.
+ * @n: natural number
+ * @i: counter or number to be multiplied.
+ *
+ * Return: sqrt 0 if sqrt is not natural.
+ */
+int sqrt_manual(int n, int i)
+{
+	if (i * i == n)
+	{
+		return (i);
+	}
+	else if (i * i > n)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (sqrt_manual(n, i + 1));
 	}
 }
