7275f044635a027e48c2a850a4b605847ad11b6f
mastax <elbahtouri.m@gmail.com>
2023-04-29 09:18:13 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include <lists.h>
+/**
+ * print_listint - print all the elements of a listint list.
+ * @h: pointer to the list header adress
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i;
+	const listint_t *temp;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	temp = h;
+	for (i = o; temp != NULL; i++)
+	{
+		printf("%d\n", temp->n);
+		temp = temp -> next;
+	}
+	return (i);
+}
