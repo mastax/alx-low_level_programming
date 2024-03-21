464393b4a06bc07887874c02053724990def52f6
mastax <elbahtouri.m@gmail.com>
2024-03-21 09:43:20 -0700

    0-print_dlistint.c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * print_dlistint - Prints all the elements of a dlistint_t list
+ * @h: Pointer to the head of the doubly linked list
+ *
+ * Return: The number of nodes in the list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+	const dlistint_t *current = h;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		count++;
+		current = current->next;
+	}
+	return (count);
+}
